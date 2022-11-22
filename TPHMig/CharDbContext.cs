3a615334d175b2e47542a3fa347eee16126a4b40
BohdanLytvynov <blitvinov19@gmail.com>
2022-11-22 21:07:39 +0200

    Successfull Init migration

---
+++ TPHMig/CharDbContext.cs
@@ -0,0 +1,32 @@
+﻿using EntityFrameWork.TPH;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPHMig
+{
+    public class CharDbContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        public DbSet<Warrior> Warriors { get; set; }
+
+        public DbSet<Hero> Heroes { get; set; }
+       
+        public CharDbContext()
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDb;Initial Catalog=TPH;Integrated Security=True");
+
+            base.OnConfiguring(optionsBuilder); 
+        }
+    }
+}
